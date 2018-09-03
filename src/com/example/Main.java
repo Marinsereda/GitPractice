90105c2dddb9784f76d2def1720d59d8f7d6fb8c
Marina <seredamv92@gmail.com>
2018-09-03 21:26:42 +0300

    Initial commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    public static String getNumbers () {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        HashMap <Integer , Integer> count = new HashMap<>();
+        for (int i=0; i<100; i++){
+           int each = (int)(Math.random()*10);
+           numbers.add(each);
+        }
+
+        for (int n : numbers){
+            Integer i = count.get(n);
+            count.put(n,  i==null ? 1 : i +1);
+        }
+        return count.toString();
+    }
+
+
+    public static void main(String[] args){
+	// write your code here
+        System.out.println(getNumbers());
+    }
+
+}
