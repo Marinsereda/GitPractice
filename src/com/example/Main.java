f4a57360a72bc6857a2f352c00d552e84a206878
Marina <seredamv92@gmail.com>
2018-09-04 15:45:50 +0300

    ts in bitbucket

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -10,19 +10,14 @@ public class Main {
         HashMap <Integer , Integer> count = new HashMap<>();
         for (int i=0; i<100; i++){
            int each = (int)(Math.random()*10);
-           numbers.add(each);
-        }
-
+           numbers.add(each); }
         for (int n : numbers){
             Integer i = count.get(n);
-            count.put(n,  i==null ? 1 : i +1);
-        }
+            count.put(n,  i==null ? 1 : i +1); }
         return count.toString();
     }
 
-
     public static void main(String[] args){
-	// write your code here
         System.out.println(getNumbers());
     }
 
